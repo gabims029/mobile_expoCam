5c5f62bad63fb07c9c7d3780f4fcab32e46ad57f
gabims029 <gabimelo541@gmail.com>
2025-09-01 11:18:30 -0300

    Novo início: remover histórico com chave secreta

---
+++ src/axios/axios.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://io.adafruit.com/api/v2/gabizinha/feeds/",
+    headers:{
+        accept: "application/json",
+        "Content-Type" : "application/json",
+        "X-AIO-Key" : "..."
+    },
+
+});
+
+const sheets = {
+    toggleLED: (stateLED) => api.post("botaoled/data", stateLED),
+};
+
+export default sheets;
